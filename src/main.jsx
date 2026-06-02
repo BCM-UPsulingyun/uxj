7866183f7a3258a01add0d4f2c39b80c9e55652c
BCM.UPsulingyun <1363084182@qq.com>
2026-06-02 20:44:52 +0800

    fix: switch to HashRouter and set base path

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,8 @@
 import React, { useState, createContext, useEffect } from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import { BrowserRouter } from 'react-router-dom'
+// 将 BrowserRouter 改为 HashRouter
+import { HashRouter } from 'react-router-dom'
 
 // 全局样式引入
 import './App.css'
@@ -54,10 +55,11 @@ export const ThemeContextProvider = ({ children }) => {
 // 渲染根组件
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
+    {/*  这里也要改成 HashRouter */}
+    <HashRouter>
       <ThemeContextProvider>
         <App />
       </ThemeContextProvider>
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 )
